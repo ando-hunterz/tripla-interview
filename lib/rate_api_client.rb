d022780e829854d5e7d916a10b571a9c6191ec9f
ando-hunterz <ando.hunterz@gmail.com>
2026-02-23 14:49:21 +0700

    feat(pricing): add get all rate and add error handling

--- lib/rate_api_client.rb
+++ lib/rate_api_client.rb
@@ -15,5 +15,24 @@ class RateApiClient
       ]
     }.to_json
     self.post("/pricing", body: params)
+  rescue StandardError => e
+    Rails.logger.error("[RateApiClient#get_rate] #{e.class}: #{e.message}")
+    raise
+  end
+
+  def self.get_all_rate
+    attributes = PricingConstants::VALID_PERIODS.flat_map do |period|
+      PricingConstants::VALID_HOTELS.flat_map do |hotel|
+        PricingConstants::VALID_ROOMS.map do |room|
+          { period: period, hotel: hotel, room: room }
+        end
+      end
+    end
+
+    params = { attributes: attributes }.to_json
+    self.post("/pricing", body: params)
+  rescue StandardError => e
+    Rails.logger.error("[RateApiClient#get_all_rate] #{e.class}: #{e.message}")
+    raise
   end
 end
