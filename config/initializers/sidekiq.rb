d31aaa179815a0189ec2c9732e5927962ef0058b
ando-hunterz <ando.hunterz@gmail.com>
2026-02-23 14:45:37 +0700

    feat(worker): add worker job

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,5 +4,9 @@ end
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch("REDIS_URL", "redis://redis:6379") }
+
+  config.on(:startup) do
+    RateCacheWorker.perform_in(10.seconds)
+  end
 end
 
