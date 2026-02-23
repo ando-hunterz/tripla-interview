63cc0bf1803d57a420ab332eac22e2d611367c4c
ando-hunterz <ando.hunterz@gmail.com>
2026-02-23 15:40:53 +0700

    feat(worker): add 3 retries every 30s

--- app/workers/rate_cache_worker.rb
+++ app/workers/rate_cache_worker.rb
@@ -2,6 +2,10 @@ class RateCacheWorker
   include Sidekiq::Worker
   sidekiq_options retry: 3
 
+  sidekiq_retry_in do |_count, _exception|
+    30
+  end
+
   def perform
     RateCacheService.new.set_rate
   ensure
