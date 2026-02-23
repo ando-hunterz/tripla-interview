a3dff281e6b32005925a3acd642390fe242778cf
ando-hunterz <ando.hunterz@gmail.com>
2026-02-23 15:09:47 +0700

    feat(worker): change waiting time to 3 seconds

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,8 +5,9 @@ end
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch("REDIS_URL", "redis://redis:6379") }
 
+  # 3 seconds waiting time, as we need to wait for the api to startup
   config.on(:startup) do
-    RateCacheWorker.perform_in(10.seconds)
+    RateCacheWorker.perform_in(3.seconds)
   end
 end
 
